97b6f9708bb3db5dd063255c0dfc7c8c4f733f73
skubcat <BriannasTalkingDrum@gmail.com>
2023-06-12 17:51:47 -0300

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -20,7 +20,7 @@ gen_header() {
     echo $HEADER
     echo "<header> <a href="https://bugmancooking.neocities.org/">[bugman.cooking]</a>"
     echo "<a href="https://bugmancooking.neocities.org/about">[about]</a>"
-    echo "<a href="https://bugmancooking.neocities.org/dlsite">[download]</a>"
+    echo "<a href="https://github.com/skubcat/carapace.sh/archive/refs/heads/main.zip">[download]</a>"
     echo "</header>" 
     
     } >> $1
